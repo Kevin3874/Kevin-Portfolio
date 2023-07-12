9afb5b8e077231faa0f7c286cfe3c56957111f21
Kevin3874 <kzhang77@jh.edu>
2023-07-12 10:38:58 -0400

    Color and style updates, removed projects page

--- kevin-portfolio/src/pages/index.tsx
+++ kevin-portfolio/src/pages/index.tsx
@@ -1,74 +1,116 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { useEffect, useState } from 'react';
 
 const Home: NextPage = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+  
   return (
     <>
       <Head>
-        <title>{"Kevin's Porfolio"}</title>
+        <title>{"Kevin's Portfolio"}</title>
         <meta name="description" content="Learn more about my experiences and projects!" />
         <meta name="author" content="Kevin Zhang" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/KleanZ.ico" />
       </Head>
-      <main className="flex h-screen flex-col items-center justify-between bg-gradient-to-b from-[#2a023f] to-[#1f4186]">
-        <div className="container flex flex-col items-center justify-center px-10 py-16 gap-10">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-5xl md:text-6xl lg:text-7xl">
-            {"Hi, I'm Kevin"}
-          </h1>
-          <div className="hero bg-base">
-            <div className="hero-content flex-col lg:flex-row max-w-5xl">
-              <img src="https://lh6.googleusercontent.com/EjVF2hhC1YbjDmeqfIsS5cMq1idMpONh8KCWlTuai-avYRjFd9AWGFXvbSqgWtHc44o=w2400" alt = "My Image"
-              className="w-32 md:w-32 lg:w-72"/>            
-              <div>
-                <h1 className="text-4xl font-bold text-white text-center sm:text-left">About Me</h1>
-                <p className="pt-1 text-white text-center sm:text-left">{"I'm a current Johns Hopkins Undergraduate studying a B.S. in Computer Science and a minor in Applied Mathematics & Statistics. I hope to continue my education by pursuing Hopkin's combined BS/MSE 4 year program."}</p>
-                <p className="pt-3 text-white text-center sm:text-left">{"Some Languages I know:"}</p>
-                <p className="text-white text-center sm:text-left">{"Python · Java · C/C++ · JavaScript/TypeScript · HTML5 & CSS · Rust · C# "}</p>
-                <p className="pt-3 text-white text-center sm:text-left">{"Technologies:"}</p>
-                <p className="pb-3 text-white text-center sm:text-left">{"Firebase · React.js · Next.js · Android Studio · Git · Node.js · Azure DevOps "}</p>
-                <Link href = "https://kevinzhang25.notion.site/Resume-Download-1fcf8411f885460cb918ef2b9873cf3b" 
-                target={"_blank"}>
-                <button className="btn bg-[#2b46aa] p-4 text-white hover:bg-white/20">My Resume</button>
-                </Link>
-                <Link href ="mailto:kzhang77@jh.edu"
-                target={"_blank"}>
-                <button className="btn bg-[#2b46aa] p-4 text-white hover:bg-white/20 ml-5">Email Me</button>
-                </Link>
-              </div>
+      <main className={`flex flex-col min-h-screen ${isLoaded ? 'animate-fade-in' : ''}`}>
+        <div className="flex-grow flex flex-row">
+          <div className="w-3/5 bg-[#1a237e] flex flex-col items-center justify-center text-white p-8">
+            <div className={isLoaded ? 'animate-slide-in' : ''}>
+              <h1 className="text-4xl font-bold mb-4 text-center">About Me</h1>
+              <p className="mb-4 text-center max-w-lg mx-auto">{"I'm a current Johns Hopkins Undergraduate studying a B.S. in Computer Science and a minor in Applied Mathematics & Statistics. I hope to continue my education by pursuing Hopkin's combined BS/MSE 4 year program."}</p>
+            </div>
+            <p className="mb-2 text-center max-w-lg mx-auto">{"Some Languages I know:"}</p>
+            <p className="mb-2 text-center max-w-lg mx-auto">{"Python · Java · C/C++ · JavaScript/TypeScript · Rust · C# "}</p>
+            <p className="mb-2 text-center max-w-lg mx-auto">{"Technologies:"}</p>
+            <p className="mb-4 text-center max-w-lg mx-auto">{"Firebase · React.js · React Native · Next.js · Git · Node.js · Azure DevOps · Angular 6"}</p>
+            <div className="flex gap-4 mb-4">
+              <Link href="https://kevinzhang25.notion.site/Resume-Download-1fcf8411f885460cb918ef2b9873cf3b"
+                    target={"_blank"}>
+                <button className="btn bg-[#f5f5dc] p-3 text-[#333] hover:bg-[#e5e5cc]">My Resume</button>
+              </Link>
+              <Link href="https://github.com/Kevin3874"
+                    target={"_blank"}>
+                <button className="btn bg-[#f5f5dc] p-3 text-[#333] hover:bg-[#e5e5cc]">Github</button>
+              </Link>
+            </div>
+            <div className="flex justify-center">
+            <Link href="mailto:kzhang77@jh.edu"
+                    target={"_blank"}>
+                <button className="btn bg-[#f5f5dc] p-3 text-[#333] hover:bg-[#e5e5cc]">Email Me</button>
+              </Link>
             </div>
           </div>
-          <div className="container flex flex-row itmes-center justify-center">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="projects">
-              <h3 className="text-2xl font-bold">Check out my projects →</h3>
-              <div className="text-lg">
-                Learn more about the teams and projects I have been invoved in.
-              </div>
-            </Link>
+          <div className="w-2/5 bg-[#f5f5dc] flex flex-col items-center justify-center text-[#333] p-8">
+            <img src="https://lh6.googleusercontent.com/EjVF2hhC1YbjDmeqfIsS5cMq1idMpONh8KCWlTuai-avYRjFd9AWGFXvbSqgWtHc44o=w2400" alt="My Image"
+               className={`w-48 md:w-72 mb-8 rounded-full shadow-lg`}/>
+            <h1 className="text-5xl font-extrabold mb-4">Kevin Zhang</h1>
+            <h2 className="text-3xl typewriter">Fullstack Software Engineer</h2>
           </div>
         </div>
-        <footer className="footer p-12 bg-neutral text-neutral-content gap-12 justify-center text-center bg-gradient-to-b from-[#020d4b] to-[#02054b] h-28">
-          <div className = "-translate-y-8 -translate-x-8 flex flex-col items-center">
-            <span className="footer-title">Email</span> 
-            <a className="text">zzsshwkevin@gmail.com</a>
-            <a className="text">kzhang77@jh.edu</a>
-          </div> 
-          <div className = "-translate-y-8 -translate-x-8 flex flex-col items-center">
-            <span className="footer-title">Phone & Cell</span> 
-            <a className="text">{"(813) 842- 6045"}</a>
-          </div> 
-          <div className = "-translate-y-8 -translate-x-8 flex flex-col items-center">
-            <span className="footer-title">LinkedIn</span> 
-            <Link href = "https://www.linkedin.com/in/kevinzhang25/"
-            target={"_blank"}>
-              <button className="link link-hover">Kevin Zhang</button>
-            </Link>
+        <footer className="bg-white text-[#333] p-4 text-center">
+          <div className="flex justify-around items-center">
+            <div className="flex flex-col items-center">
+              <span className="footer-title">Email</span>
+              <a className="text">zzsshwkevin@gmail.com</a>
+              <a className="text">kzhang77@jh.edu</a>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="footer-title">Phone & Cell</span>
+              <a className="text">{"(813) 842- 6045"}</a>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="footer-title">LinkedIn</span>
+              <Link href="https://www.linkedin.com/in/kevinzhang25/"
+                    target={"_blank"}>
+                <button className="link link-hover">Kevin Zhang</button>
+              </Link>
+            </div>
           </div>
         </footer>
       </main>
+      <style jsx>{`
+        .animate-fade-in {
+          animation: fadeIn 3s;
+        }
+        .animate-slide-in {
+          animation: slideIn 3s;
+        }
+        .typewriter {
+          overflow: hidden;
+          border-right: .15em solid orange;
+          white-space: nowrap;
+          margin: 0 auto;
+          letter-spacing: .15em;
+          animation: typing 3.5s steps(40, end), blink-caret .75s step-end infinite;
+        }
+        @keyframes fadeIn {
+          from { opacity: 0; }
+          to { opacity: 1; }
+        }
+        @keyframes slideIn {
+          from { transform: translateY(-50px); opacity: 0; }
+          to { transform: translateY(0); opacity: 1; }
+        }
+        @keyframes spin {
+          0% { transform: rotate(0deg); }
+          100% { transform: rotate(360deg); }
+        }
+        @keyframes typing {
+          from { width: 0; }
+          to { width: 100%; }
+        }
+        @keyframes blink-caret {
+          from, to { border-color: transparent; }
+          50% { border-color: orange; }
+        }
+      `}</style>
     </>
   );
 };
--- kevin-portfolio/src/pages/projects.tsx
+++ kevin-portfolio/src/pages/projects.tsx
@@ -2,7 +2,7 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 
-const Home: NextPage = () => {
+const Projects: NextPage = () => {
   return (
     <>
       <Head>
@@ -10,41 +10,46 @@ const Home: NextPage = () => {
         <meta name="description" content="Kevin Project Page" />
         <link rel="icon" href="/KleanZ.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            <Link href = {"/"}>
-              <span className="text-[hsl(280,100%,70%)]"> {"Kevin's"} </span>
-            </Link>
-            Projects
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://github.com/Kevin3874"
-              target="_blank"
-            >
+      <main className="flex flex-col min-h-screen bg-[#1a237e] text-white p-8 items-center justify-center">
+        <h1 className="text-5xl font-extrabold tracking-tight mb-8 text-center">
+          <Link href={"/"}>
+            <span className="text-[#dcdcad] hover:text-[#7d7d61] cursor-pointer shadow-text">{"Kevin's "}</span>
+          </Link>
+          Projects
+        </h1>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 w-full max-w-3xl">
+          <Link
+            href="https://github.com/Kevin3874"
+            target="_blank"
+          >
+            <div className="flex flex-col items-center gap-4 p-4 bg-white/10 rounded-xl hover:bg-white/20 cursor-pointer">
               <h3 className="text-2xl font-bold">Github →</h3>
-              <div className="text-lg">
+              <p className="text-lg text-center">
                 Link to my public Github repository with all of the projects I have worked on!
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://kevinzhang25.notion.site/kevinzhang25/Kevin-Zhang-80f3a8cf8e704dc4bede040bc411037e"
-              target="_blank"
-            >
+              </p>
+            </div>
+          </Link>
+          <Link
+            href="https://kevinzhang25.notion.site/kevinzhang25/Kevin-Zhang-80f3a8cf8e704dc4bede040bc411037e"
+            target="_blank"
+          >
+            <div className="flex flex-col items-center gap-4 p-4 bg-white/10 rounded-xl hover:bg-white/20 cursor-pointer">
               <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Link to my public Notion page where I go more in-depth
+              <p className="text-lg text-center">
+                Link to my Notion page where I go more in-depth
                 about the details of each project!
-              </div>
-            </Link>
-          </div>
+              </p>
+            </div>
+          </Link>
         </div>
       </main>
+      <style jsx>{`
+        .shadow-text {
+          text-shadow: 2px 2px 2px #000;
+        }
+      `}</style>
     </>
   );
 };
 
-export default Home;
+export default Projects;
