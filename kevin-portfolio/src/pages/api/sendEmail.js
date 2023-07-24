489c0a5a9c14a3e42fb9012e4ba85767db7dc6cf
Kevin3874 <kzhang77@jh.edu>
2023-07-24 15:08:47 -0400

    mail service change

--- kevin-portfolio/src/pages/api/sendEmail.js
+++ kevin-portfolio/src/pages/api/sendEmail.js
@@ -1,32 +1,30 @@
-import sgMail from '@sendgrid/mail';
+import mailgun from 'mailgun-js';
 
 export default async function handler(req, res) {
   const { email, message, subject } = req.body;
 
-  const sendGridApiKey = process.env.SENDGRID_API_KEY;
+  const mailgunApiKey = process.env.MAILGUN_API_KEY;
+  const mailgunDomain = process.env.MAILGUN_DOMAIN;
   const emailUsername = process.env.EMAIL_USERNAME;
   const emailSendUsername = process.env.EMAILSEND_USERNAME;
 
-  if (!sendGridApiKey || !emailUsername || !emailSendUsername) {
+  if (!mailgunApiKey || !mailgunDomain || !emailUsername || !emailSendUsername) {
     console.error('One or more environment variables are not defined');
     res.status(500).json({ success: false });
     return;
   }
 
-  sgMail.setApiKey(sendGridApiKey);
+  const mg = mailgun({ apiKey: mailgunApiKey, domain: mailgunDomain });
 
-  const msg = {
+  const data = {
+    from: 'kevinzhang.dev <' + emailSendUsername + '>',
     to: emailUsername,
-    from: {
-      name: 'kevinzhang.dev',
-      email: emailSendUsername,
-    },
     subject: subject,
     text: "From: " + email + "\n" + "Message: " + "\n" + message
   };
 
   try {
-    await sgMail.send(msg);
+    await mg.messages().send(data);
     res.status(200).json({ success: true });
   } catch (error) {
     console.error('Error sending email:', error);
